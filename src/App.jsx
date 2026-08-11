3812f56c635f3cdbf94cdb60f20280ecfaeac698
Aluno <e023aluno@outlook.com>
2026-08-11 20:02:52 -0300

    FormProduto

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import FormProduto from "./Componentes/FormProduto";
+
+export default function App(){
+
+  return(
+   <>
+    <main className="container">
+      <FormProduto/>
+
+
+    </main>
+   
+   </>
+
+
+  )
+}
